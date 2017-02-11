ab8b02cc1ded7da143e824461d5b1e5481b8a3a6
Rita Bulman <ritabulman@outlook.com>
2017-02-10 23:31:37 -0500

    questions rerender in random order onClick continue

--- src/components/quiz/QuizNewQuestions.js
+++ src/components/quiz/QuizNewQuestions.js
@@ -6,18 +6,20 @@ import QuizResult from './QuizResult';
 export default class NewQuestions extends React.Component {
     constructor(props) {
         super(props);
+
         this.state = {
             score: 0,
             question: '',
             questionsAsked: {}
         };
+
         this.randomQuestion = this.randomQuestion.bind(this);
     }
     
-    componentDidMount() { 
+    componentDidMount() { /* eslint react/no-did-mount-set-state: 0 */
         this.setState({
             question: this.randomQuestion(),
-            questionsAsked: Object.assign({}, this.state.questionsAsked, this.question)
+            questionsAsked: {}
         });
     }
 
@@ -25,9 +27,14 @@ export default class NewQuestions extends React.Component {
         //grabs 1 random question from array
         let question = this.props.questions[Math.floor(Math.random() * this.props.questions.length)];
         //if the question has been asked, ask a different one
-        if(!this.state.questionsAsked[question.id]) {
-            this.setState({questionsAsked: Object.assign({}, this.state.questionsAsked, question)});
-            console.log('this.state.question: ', this.state.questionsAsked);
+        console.log('QUESTION: ', question);
+        console.log('this.state ----', this.state);
+        if(!this.state.questionsAsked[question.id]) { 
+            this.setState({
+                question: question,
+                questionsAsked: Object.assign({}, this.state.questionsAsked, question)
+            });
+            console.log('this.state.questionsAsked: ', this.state.questionsAsked);
             return question;
         }
         else {
@@ -41,11 +48,11 @@ export default class NewQuestions extends React.Component {
                 {
                     Object.keys(this.state.questionsAsked).length <= 7 ?
                         <div>
-                            <p> {this.state.question.id}</p>
+                            <p> {this.state.question[Object.keys(this.state.question)[0]].q}</p>
                             <button className="btn btn-primary" onClick={this.randomQuestion}>继续</button>
                         </div>
                     :
-                        <p> reached 8, will show quiz results, a link to learn more about that part of speech</p>
+                        <p>reached 8, will show quiz results, a link to learn more about that part of speech</p>
                 }
                     
                 
@@ -54,6 +61,8 @@ export default class NewQuestions extends React.Component {
     }
 }
 
-
+NewQuestions.propTypes = {
+    questions: PropTypes.array
+};
 
 

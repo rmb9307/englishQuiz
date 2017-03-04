ab1b0cecbc2a19bf7453c30ec12ea09229405e30
Rita Bulman <ritabulman@outlook.com>
2017-03-03 19:26:43 -0500

    Update 2017-03-04T00:26:24.220Z

--- src/actions/actionTypes.js
+++
@@ -1,4 +0,0 @@
-export const BEGIN_AJAX_CALL = 'BEGIN_AJAX_CALL';
-export const AJAX_CALL_ERROR = 'AJAX_CALL_ERROR';
-
-export const SET_QUIZ = 'SET_QUIZ';
--- src/actions/quizActions.js
+++
@@ -1,7 +0,0 @@
-import * as types from './actionTypes';
-import {partsOfSpeech} from '../data/quizQuestions';
-
-
-export function setQuiz(quiz) {
-    return {type: types.SET_QUIZ, quiz};
-}

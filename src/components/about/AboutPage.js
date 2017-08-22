45a91ba49571a4ef6ee5425535351e0f62f3e47c
Rita Bulman <ritabulman@outlook.com>
2017-08-21 21:04:12 -0400

    removed english on signin and login

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -5,8 +5,6 @@ import { Link, browserHistory } from 'react-router';
 import { partsOfSpeech } from '../../data/quizQuestions';
 import * as quizActions from '../../actions/quizActions';
 
-//() => this.props.actions.setQuiz(quizKey)}>{quizKey
-
 
 const quizKeys = Object.keys(partsOfSpeech);
 
@@ -18,18 +16,23 @@ class AboutPage extends React.Component {
     
     render() {
         return (
-            <div className="text-center">
-                <h1>您想学什么?</h1>
-                <h4>测试你的语法知识:</h4> {/* onHover, reveal the parts of speech that link to each quiz*/}
-                {quizKeys.map(quizKey => {
-                    return (
-                       <p key={quizKey}><Link to={'/quiz/' + quizKey} key={quizKey}> {quizKey} </Link></p>
-                    );
-                })}
-                <h4>惯用语法</h4>
-                <h4>发音</h4>
-                <h4>听力</h4>
-                <h4>阅读</h4>
+            <div>
+                <div className="spacer"/>
+                <div className="container-fluid"> {/* The main question will be on the left, then a vertical line. On the right side, a list of topics. When you hover on a topic, I will use animation library to reveal sub-topics. */}
+                    <div className="row">
+                        <div className="col-sm-3">
+                            <h1>您想学什么?</h1>
+                        </div>
+                        <div className="col-sm-9" id="subject-list">
+                            <h4 className="practice-topic">测试你的语法知识</h4> 
+                            <h4 className="practice-topic">惯用语法</h4>
+                            <h4 className="practice-topic">发音</h4>
+                            <h4 className="practice-topic">听力</h4>
+                            <h4 className="practice-topic">阅读</h4>
+                        </div>
+                    </div>
+                
+                </div>
             </div>
         );
     }
@@ -38,3 +41,10 @@ class AboutPage extends React.Component {
 
 
 export default AboutPage;
+
+
+// {quizKeys.map(quizKey => {
+//     return (
+//     <p key={quizKey}><Link to={'/quiz/' + quizKey} key={quizKey}> {quizKey} </Link></p>
+//     );
+// })}
--- src/components/login/LoginPage.js
+++ src/components/login/LoginPage.js
@@ -4,36 +4,40 @@ import { browserHistory, Link } from 'react-router';
 class LoginPage extends React.Component {
     render() {
         return (
-            <div className="jumbotron text-center">
-
-                <h2> Login (登录) </h2>
-                <form onSubmit={evt => {
-                    evt.preventDefault();
-                    console.log('login form submitted');}}>
-                    {/*Email input*/}
-                    <div className="row">
-                        <div className="col-xs-12">
-                            <input placeholder="email (电子邮件地址)"/>
-                        </div>
-                    </div>
+            <div>
+                <div className="spacer2"/>
+                <div className="jumbotron text-center">
                     
-                    {/*Password input*/}
-                    <div className="row">
-                        <div className="col-xs-12">
-                             <input placeholder="password (密码)"/>
+
+                    <h2> 登录 </h2>
+                    <form onSubmit={evt => {
+                        evt.preventDefault();
+                        console.log('login form submitted');}}>
+                        {/*Email input*/}
+                        <div className="row">
+                            <div className="col-xs-12">
+                                <input placeholder="电子邮件地址"/>
+                            </div>
                         </div>
-                    </div>
-
-                    {/*Submit button*/}
-                    <div className="row">
-                        <div className="col-xs-12">
-                            <input className="btn" type="submit"/>
+                        
+                        {/*Password input*/}
+                        <div className="row">
+                            <div className="col-xs-12">
+                                <input placeholder="密码"/>
+                            </div>
                         </div>
-                    </div>
-                </form>    
 
-                <p><Link to="/signup"> Need a new account? Sign up here </Link></p>
+                        {/*Submit button*/}
+                        <div className="row">
+                            <div className="col-xs-12">
+                                <input className="btn" type="submit"/>
+                            </div>
+                        </div>
+                    </form>    
 
+                    <p id="signup-link"><Link to="/signup"> 需要新帐号吗? 注册  </Link></p>
+
+                </div>
             </div>
         );
     }

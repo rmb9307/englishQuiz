983e2d63a36abee2dc7395cdeaec0cfab850b087
Rita Bulman <ritabulman@outlook.com>
2017-08-31 14:46:25 -0400

    starter for dashboard page

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -6,15 +6,21 @@ class Dashboard extends React.Component {
     render() {
         return (
             <div className="container-fluid">
-                Dashboard
+                <div className="spacer2"/>
+                <h2>帐户: { this.props.user }</h2>
+                <h4> 您已完成以下测验: </h4>
             </div>
         );
     }
 }
 
+const mapStateToProps = (state, ownProps) => ({
+    user: state.user
+});
 
 Dashboard.propTypes = {
+    user: PropTypes.string
 };
 
 
-export default connect()(Dashboard);
+export default connect(mapStateToProps)(Dashboard);

f35b3823e7b7f872273a3fa677c63ad1c4042227
Rita Bulman <ritabulman@outlook.com>
2018-04-13 22:01:53 -0400

    added some letters for the pronunciation page

--- src/components/pronunciation/PronunciationPage.js
+++ src/components/pronunciation/PronunciationPage.js
@@ -7,6 +7,27 @@ import { Link, browserHistory } from 'react-router';
 import { partsOfSpeech } from '../../data/quizQuestions';
 import * as quizActions from '../../actions/quizActions';
   
+const alphabet = [          // import this from db later on
+    'a', 'b', 'c', 'd',
+    'e', 'f', 'g', 'h',
+    'i', 'j', 'k', 'l',
+    'm', 'n', 'o', 'p',
+    'q', 'r', 's', 't',
+    'u', 'v', 'w', 'x',
+    'y', 'z'
+];
+
+const vowelCombos = [       // will add more and import from db
+    'oa', 'oe', 'ow', 'ie',
+    'ey', 'ay', 'ee', 'oo', 
+    'ou', 'ew', 'au', 'ough'
+];
+
+const consonantCombos = [   // will add more and import from db
+    'th', 'sh', 'ph', 'wh',
+    'ti', 'ch'
+];
+
 class PronunciationPage extends React.Component {
      constructor(props, context) {
         super(props, context);
@@ -14,11 +35,22 @@ class PronunciationPage extends React.Component {
 
     
     render() {
+        const alphabetRows = alphabet.forEach(letter => {
+            return <td>{letter}</td>;
+        });
+        const vowelRows = vowelCombos.forEach(combo => {
+            return <td>{combo}</td>;
+        });
+        const consonantRows = consonantCombos.forEach(combo => {
+            return <td>{combo}</td>;
+        });
         return (
             <div>
-                <h1> Pronunciation Page </h1>
-                <h4> Instructions on how to use </h4>
-                
+                <h1> 听力 </h1>
+                <h4> (Add instructions on how to use) </h4>
+                <table>
+                    <tbody></tbody>
+                </table>
             </div>
         );
     }

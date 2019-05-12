3ef4d00e4e9ee0a48f0e22abd7cb6c4a385acee8
Rita Bulman <ritabulman@outlook.com>
2019-05-12 14:37:32 -0400

    node package vulnerabilities

--- src/components/pronunciation/PronunciationPage.js
+++ src/components/pronunciation/PronunciationPage.js
@@ -51,8 +51,6 @@ class PronunciationPage extends React.Component {
      constructor(props, context) {
         super(props, context);
     }
-
-    
     render() {
         return (
             <div>

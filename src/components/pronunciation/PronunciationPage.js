f7fb722835c85dd9b6d7db9b538ca338399e8f4a
Rita Bulman <ritabulman@outlook.com>
2019-05-24 22:16:21 -0400

    styles

--- src/components/pronunciation/PronunciationPage.js
+++ src/components/pronunciation/PronunciationPage.js
@@ -7,28 +7,7 @@ import { Link, browserHistory } from 'react-router';
 import { partsOfSpeech } from '../../data/quizQuestions';
 import * as quizActions from '../../actions/quizActions';
 import _ from 'lodash';
-  
-const alphabet = [          // import this from db later on
-    'a', 'b', 'c', 'd',
-    'e', 'f', 'g', 'h',
-    'i', 'j', 'k', 'l',
-    'm', 'n', 'o', 'p',
-    'q', 'r', 's', 't',
-    'u', 'v', 'w', 'x',
-    'y', 'z'
-];
-
-const vowelCombos = [       // will add more and import from db
-    'oa', 'oe', 'ow', 'ie',
-    'ey', 'ay', 'ee', 'oo', 
-    'ou', 'ew', 'au', 'ough',
-    'eigh'
-];
-
-const consonantCombos = [   // will add more and import from db
-    'th', 'sh', 'ph', 'wh',
-    'ti', 'ch'
-];
+import {alphabet, vowelCombos, consonantCombos} from './constants/letters';
 
 let gridElements = _.chunk(
     [...alphabet, ...vowelCombos, ...consonantCombos],
@@ -36,7 +15,7 @@ let gridElements = _.chunk(
 );
 gridElements.forEach((rowContent, index) => {
     gridElements[index] = rowContent.map((cellText, index) => {
-        return <td key={index}>{cellText}</td>;
+        return <td key={index} className="hand-pointer sound-grid-cell">{cellText}</td>;
     });
 });
 gridElements = gridElements.map((rowContent, index) => {
@@ -51,17 +30,17 @@ class PronunciationPage extends React.Component {
      constructor(props, context) {
         super(props, context);
     }
-
-    
     render() {
         return (
             <div>
                 <h1> 发音 </h1>
-                <table className="sound-grid">
-                    <tbody>
-                        {gridElements}
-                    </tbody>
-                </table>
+                <div className="sound-grid-container">
+                    <table className="sound-grid">
+                        <tbody>
+                            {gridElements}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         );
     }
